57a04a82f08cd9c9a444228820dd623e876c5ad5
André Ribeiro <andrepedro2004@hotmail.com>
2023-11-12 18:20:17 +0000

    remove program_name=av[0], erro professor em todo o lado, parece varicela

--- imageTest.c
+++ imageTest.c
@@ -11,32 +11,28 @@
 
 #include <assert.h>
 #include <errno.h>
-<<<<<<< HEAD
-#include <error.h>
-=======
 #include "error.h"
->>>>>>> upstream/main
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "image8bit.h"
 #include "instrumentation.h"
 
-int main(int argc, char* argv[]) {
-<<<<<<< HEAD
-=======
-  program_name = argv[0];
->>>>>>> upstream/main
-  if (argc != 3) {
+int main(int argc, char *argv[])
+{
+  // program_name = argv[0];
+  if (argc != 3)
+  {
     error(1, 0, "Usage: imageTest input.pgm output.pgm");
   }
 
   ImageInit();
-  
+
   printf("# LOAD image");
   InstrReset(); // to reset instrumentation
   Image img1 = ImageLoad(argv[1]);
-  if (img1 == NULL) {
+  if (img1 == NULL)
+  {
     error(2, errno, "Loading %s: %s", argv[1], ImageErrMsg());
   }
   InstrPrint(); // to print instrumentation
@@ -44,17 +40,19 @@ int main(int argc, char* argv[]) {
   // Try changing the behaviour of the program by commenting/uncommenting
   // the appropriate lines.
 
-  //img2 = ImageCrop(img1, ImageWidth(img1)/4, ImageHeight(img1)/4, ImageWidth(img1)/2, ImageHeight(img1)/2);
+  // img2 = ImageCrop(img1, ImageWidth(img1)/4, ImageHeight(img1)/4, ImageWidth(img1)/2, ImageHeight(img1)/2);
   Image img2 = ImageRotate(img1);
-  if (img2 == NULL) {
+  if (img2 == NULL)
+  {
     error(2, errno, "Rotating img2: %s", ImageErrMsg());
   }
 
-  //ImageNegative(img2);
-  //ImageThreshold(img2, 100);
+  // ImageNegative(img2);
+  // ImageThreshold(img2, 100);
   ImageBrighten(img2, 1.3);
 
-  if (ImageSave(img2, argv[2]) == 0) {
+  if (ImageSave(img2, argv[2]) == 0)
+  {
     error(2, errno, "%s: %s", argv[2], ImageErrMsg());
   }
 
@@ -62,4 +60,3 @@ int main(int argc, char* argv[]) {
   ImageDestroy(&img2);
   return 0;
 }
-
